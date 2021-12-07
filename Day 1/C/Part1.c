6e2b8fdcbdb74d8c50173415397bd504f360bab1
VictorStankov <viktorstankov1@gmail.com>
2021-12-07 22:01:05 +0200

    Reorganise and revamp code

---
+++ Day 1/C/Part1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, previous = -1, counter = 0;
+    FILE* input = fopen("../input.txt", "r");
+    if (input == NULL)
+	return 1;
+
+    while (fscanf(input, "%d", &num))
+    {
+	if (feof(input))
+	    break;
+        if (num > previous && previous != -1)
+            counter++;
+        
+        previous = num;
+    }
+
+    printf("%d\n", counter);
+    return 0;
+}
--- Day 1/C/Part2.c
+++ Day 1/C/Part2.c
@@ -3,10 +3,16 @@
 int main()
 {
     int numbers[3] = {0, 0, 0}, previous = 0, sum = 0, counter = 0;
+    FILE* input = fopen("../input.txt", "r");
+    if (input == NULL)
+	return 1;
 
-    while (scanf("%d", &numbers[0]))
+    while (fscanf(input, "%d", &numbers[0]))
     {
-        if (numbers[0] && numbers[1] && numbers[2])
+	if (feof(input))
+	    break;
+
+	if (numbers[0] && numbers[1] && numbers[2])
             sum = numbers[0] + numbers[1] + numbers[2];
             
         if (previous && sum > previous)
@@ -18,4 +24,5 @@ int main()
     }
 
     printf("%d\n", counter);
+    return 0;
 }
