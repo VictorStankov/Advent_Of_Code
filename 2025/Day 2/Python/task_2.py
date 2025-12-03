41ece4f2bca5181ca924605b27471124ee8bd28b
VictorStankov <viktorstankov1@gmail.com>
2025-12-03 10:19:40 +0200

    refactor: Put solution in a function

--- 2025/Day 2/Python/task_2.py
+++ 2025/Day 2/Python/task_2.py
@@ -1,28 +1,32 @@
-with open('../input.txt', 'r') as file:
-    line = file.readline()
-
-result = 0
 prime_nums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
 
-for code_pair in line.split(','):
-    start, end = code_pair.split('-')
 
-    for num in range(int(start), int(end) + 1):
-        str_num = str(num)
-        length = len(str_num)
+def solution(input_line: str):
+    result = 0
 
-        # The idea is we check if the length of the ID is divisible by a prime number. If it is, we check whether the
-        # first N characters repeated by that prime number is the same as the original string, where N is the length of
-        # the string divided by the prime number
-        for prime_num in prime_nums:
-            if prime_num > length:
-                break
+    for code_pair in input_line.split(','):
+        start, end = code_pair.split('-')
 
-            if length % prime_num != 0:
-                continue
+        for num in range(int(start), int(end) + 1):
+            str_num = str(num)
+            length = len(str_num)
 
-            if str_num[:int(length // prime_num)] * prime_num == str_num:
-                result += num
-                break
+            # The idea is we check if the length of the ID is divisible by a prime number. If it is, we check whether
+            # the first N characters repeated by that prime number is the same as the original string, where N is the
+            # length of the string divided by the prime number
+            for prime_num in prime_nums:
+                if prime_num > length:
+                    break
 
-print(result)
+                if length % prime_num != 0:
+                    continue
+
+                if str_num[:length // prime_num] * prime_num == str_num:
+                    result += num
+                    break
+
+if __name__ == '__main__':
+    with open('../input.txt', 'r') as file:
+        line = file.readline()
+
+    print(solution(line))
